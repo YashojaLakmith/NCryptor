8020f08601adb2f6781b2bef603662db36614b6e
YashojaLakmith <yashoja002@gmail.com>
2024-01-07 00:28:15 +0530

    Implemented P/Invokes and app initialization

---
+++ NCryptor.GUI/Helpers/ExternalMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NCryptor.GUI.Helpers
+{
+    internal class ExternalMethods
+    {
+        private ExternalMethods() { }
+
+        internal static bool CmpByteArrays(byte[] a, byte[] b)
+        {
+            return a.Length == b.Length && memcmp(a, b, a.Length) == 0;
+        }
+
+        internal static void ZeroMemset(byte[] a)
+        {
+            memset(a, 0, a.Length);
+        }
+
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int memcmp(byte[] a, byte[] b, long size);
+
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr memset(byte[] a, int ch, long size);
+    }
+}
--- NCryptor.GUI/Program.cs
+++ NCryptor.GUI/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Threading;
 using System.Windows.Forms;
 
 using NCryptor.GUI.Forms;
@@ -13,9 +18,48 @@ namespace NCryptor.GUI
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainWindow());
+            string appGuid = ((GuidAttribute)Assembly.GetExecutingAssembly()
+                                                        .GetCustomAttributes(typeof(GuidAttribute), false)
+                                                        .GetValue(0)).Value.ToString();
+            string mutexId = $"Global\\{appGuid}";
+            bool createdNew;
+            var rule = new MutexAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null),
+                                            MutexRights.FullControl,
+                                            AccessControlType.Allow);
+            var mutexSettings = new MutexSecurity();
+            mutexSettings.AddAccessRule(rule);
+
+            using(var mutex = new Mutex(false, mutexId, out createdNew, mutexSettings))
+            {
+                bool hasHandle = false;
+                try
+                {
+                    try
+                    {
+                        hasHandle = mutex.WaitOne(1000, false);
+                        if (!hasHandle)
+                        {
+                            MessageBox.Show("An instance of the application is already running.", "NCryptor", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                    }
+                    catch (AbandonedMutexException)
+                    {
+                        hasHandle = true;
+                    }
+
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new MainWindow());
+                }
+                finally
+                {
+                    if (hasHandle)
+                    {
+                        mutex.ReleaseMutex();
+                    }
+                }
+            }
         }
     }
 }
